8e1699b0a0614aaa4bcd12d689481f33fb4812b4
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2023-11-27 21:48:08 +0500

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,15 @@
+//compute the sum, assign to result variable and Print the sum of all three integers
+
+#include<iostream>
+using namespace std;
+int main()
+	{
+	
+	int x, y, z;
+	x=2, y=7, z=4;
+	int result = x*y*z;
+	
+	cout << "The product of three numbers 2, 7 and 4 is = " << result << endl;
+	
+	return 0;	
+}
